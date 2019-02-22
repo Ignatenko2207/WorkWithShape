2f0c5ae467490040f459deedd229192af2b8eed3
Alex <ignatenko2207@gmail.com>
2019-02-22 22:20:31 +0200

    Initial commit

---
+++ src/ua/com/qalight/square/Square.java
@@ -0,0 +1,28 @@
+package ua.com.qalight.square;
+
+import ua.com.qalight.abstraction.AbstractShape;
+
+public class Square extends AbstractShape{
+
+	int side;
+
+	public int getSide() {
+		return side;
+	}
+
+	public void setSide(int side) {
+		this.side = side;
+	}
+
+	@Override
+	public int getArea() {
+		return side*side;
+	}
+
+	public Square(int side) {
+		this.side = side;
+	}
+
+	public Square() {
+	}
+}
